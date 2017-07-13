68682ca5ad3cb7ed54b3dfc65b6286246817c58c
Cleber Varçal <cleber.varcal@outlook.com>
2017-07-13 12:03:58 -0300

    Initial commit

---
+++ src/Api/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Api.Security;
+
+namespace Api.Controllers
+{
+    public class AccountController : Controller
+    {
+        public async Task<IActionResult> Authenticate()
+        {
+            return new OkResult();
+        }
+
+        private static void ThrowIfInvalidOptions(TokenOptions options)
+        {
+            if (options == null) throw new ArgumentNullException(nameof(options));
+
+            if (options.ValidFor <= TimeSpan.Zero)
+                throw new ArgumentException("O período deve ser maior que zero", nameof(TokenOptions.ValidFor));
+
+            if (options.SigningCredentials == null)
+                throw new ArgumentNullException(nameof(TokenOptions.SigningCredentials));
+
+            if (options.JtiGenerator == null)
+                throw new ArgumentNullException(nameof(TokenOptions.JtiGenerator));
+        }
+
+        private static long ToUnixEpochDate(DateTime date) => (long)Math.Round((date.ToUniversalTime() - new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero)).TotalSeconds);
+    }
+}
---
+++ src/Api/Startup.cs
@@ -0,0 +1,111 @@
+﻿using Api.Security;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Text;
+
+namespace Api
+{
+    public class Startup
+    {
+        private const string ISSUER = "c1f51f42";
+        private const string AUDIENCE = "c6bbbb645024";
+        private const string SECRET_KEY = "c1f51f42-5727-4d15-b787-c6bbbb645024";
+
+        private readonly SymmetricSecurityKey _signingKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(SECRET_KEY));
+
+        public IConfigurationRoot Configuration { get; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton(Configuration);
+            services.AddCors();
+
+            services.AddMvc(config =>
+            {
+                var policy = new AuthorizationPolicyBuilder()
+                       .RequireAuthenticatedUser()
+                       .Build();
+                config.Filters.Add(new AuthorizeFilter(policy));
+            });
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("User", policy => policy.RequireClaim("ApiDemo", "User"));
+                options.AddPolicy("Admin", policy => policy.RequireClaim("ApiDemo", "Admin"));
+            });
+
+            services.Configure<TokenOptions>(options =>
+            {
+                options.Issuer = ISSUER;
+                options.Audience = AUDIENCE;
+                options.SigningCredentials = new SigningCredentials(_signingKey, SecurityAlgorithms.HmacSha256);
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            loggerFactory.AddDebug();
+
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidIssuer = ISSUER,
+
+                ValidateAudience = true,
+                ValidAudience = AUDIENCE,
+
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = _signingKey,
+
+                RequireExpirationTime = true,
+                ValidateLifetime = true,
+
+                ClockSkew = TimeSpan.Zero
+            };
+
+            app.UseJwtBearerAuthentication(new JwtBearerOptions
+            {
+                AutomaticAuthenticate = true,
+                AutomaticChallenge = true,
+                TokenValidationParameters = tokenValidationParameters
+            });
+
+            app.UseCors(x =>
+            {
+                x.AllowAnyHeader();
+                x.AllowAnyMethod();
+                x.AllowAnyOrigin();
+            });
+
+            app.UseMvc();
+        }
+    }
+}
